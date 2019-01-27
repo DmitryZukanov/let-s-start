10f5bc94a7e7d0fc3d7d00b507b9e3375ba25bd6
vladi <tsukanov.vladislav@gmail.com>
2019-01-27 13:14:10 +0300

    jpa-annotations

--- umldata/src/main/java/ru/umldata/model/UmlClass.java
+++ umldata/src/main/java/ru/umldata/model/UmlClass.java
@@ -1,9 +1,30 @@
 package ru.umldata.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NamedQueries({
+        @NamedQuery(name = UmlClass.ALL_SORTED, query = "SELECT m FROM UmlClass c WHERE c.id=:id"),
+        @NamedQuery(name = UmlClass.DELETE, query = "DELETE FROM UmlClass c WHERE c.id=:id"),
+})
+@Entity
+@Table(name = "uml_class")
 public class UmlClass {
+
+    public static final String ALL_SORTED = "Meal.getAll";
+    public static final String DELETE = "Meal.delete";
+
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @NotNull
     private Long id;
+
+    @Column(name = "pub_pri")
     private String pubPri;
+
     private int isStatic;
+
     private int typeIsInner;
 
 
